ac93344733a1d8d80e0d287f6efa1f7a94937437
Brian Anderson <bqanderson@me.com>
2017-03-04 23:57:12 -0600

    Small fixes

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,4 +2,19 @@ class CharactersController < ApplicationController
   def index
     @characters = Character.all
   end
+
+  def create
+    @character = Character.new(character_params)
+
+    if @character.save
+      render json: @character
+    else
+      render json: @character.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def character_params
+      params.require(:character).permit(:name, :class_type, :level, :race, :alignment)
+    end
 end
